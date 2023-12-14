a149084f2eb9952566d324c4f1deffd16d0ed445
augshailk <augsahilk@gmail.com>
2023-12-14 10:59:40 +0530

    Basics

---
+++ S02L02-Basic-Element/index.js
@@ -0,0 +1,59 @@
+// console.log("sahil");
+// alert("learning");
+
+var x = 10;
+console.log(x);
+
+x = "sahil";
+console.log(x);
+
+let y = 10;
+console.log(y);
+
+y = "sahil";
+console.log(y);
+
+const Z = 15;
+console.log(Z);
+// Z = 35;//we can't change const
+console.log(Z);
+
+var a = 'Hello there'; //String
+var b = 33.5; //Number
+var flag = true; //Boolean
+var c; //Undefined
+var d = null; //null
+var person = {"name": "John", "age": "24"}; //Object
+var cities = ["Mumbai", "Dublin", "NY"]; //Array
+var greeting = function(){
+    return "Hello";
+} //function
+alert(typeof greeting);
+
+let k;
+k = 38+8;
+console.log(k)
+
+k = 89-3;
+console.log(k)
+
+k = 89*3;
+console.log(k)
+
+k = 758/7;
+console.log(k)
+
+k = 873%7;
+console.log(k)
+k = 10**2;
+console.log(k)
+k = 10**3;
+console.log(k)
+ let text = "my name,is.sahil";
+ console.log(text.split('.'));
+ console.log(text.split('a'));
+ console.log(text.slice(1,5));
+ console.log(text.substring(1,5));
+ console.log(text.slice(5));
+ console.log(text.slice(-5));//substring don't accept -ve notation.
+ console.log(text.slice[2,5]);//undefined
